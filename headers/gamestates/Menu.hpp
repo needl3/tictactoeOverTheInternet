d389d532e6a418460d4e127bfa5a09586e725267
needl3 <anishchapagai0@gmail.com>
2021-10-31 23:26:36 +0545

    sounds added and width,height bug resolved

--- headers/gamestates/Menu.hpp
+++ headers/gamestates/Menu.hpp
@@ -10,9 +10,19 @@ class CMenu{
 	private:
 		sf::RenderWindow *_window;
 		sf::Event *_event;
+
 		sf::Font _txt_font;
-		sf::SoundBuffer *_s_buffer = new sf::SoundBuffer;
-		sf::Sound *_s_sound = new sf::Sound;
+		
+		sf::SoundBuffer *_s_buffer_main = new sf::SoundBuffer;
+		sf::SoundBuffer *_s_buffer_option_hover = new sf::SoundBuffer;
+		
+		sf::Sound *_s_sound_main = new sf::Sound;
+		sf::Sound *_s_sound_other = new sf::Sound;
+		
+		sf::Texture _background_texture;
+		sf::Sprite _background_sprite;
+
+		bool _hover_played;
 
 		float _WIDTH=1080.0f, _HEIGHT=800.0f, _hover_offset=0.0f;
 		float _t_posX, _t_posY;
@@ -24,7 +34,6 @@ class CMenu{
 			sf::Vector2f r_size;
 			sf::Vector2f r_position;
 			sf::Color b_color, t_color;
-			unsigned int t_size;
 		} _items[4];
 
 		sf::Text _text_obj;
@@ -34,40 +43,47 @@ class CMenu{
 		CMenu(sf::RenderWindow& window, sf::Event& event){
 			_window = &window;
 			_event = &event;
-			_s_buffer->loadFromFile("assets/audio/theme.wav");
-			_s_sound->setBuffer(*_s_buffer);
-			_s_sound->setLoop(true);
-			
+
+			_WIDTH = _window->getSize().x;
+			_HEIGHT = _window->getSize().y;
+
+//------------------------------------------------------------------
+			// TODO FIX SPRITE WHITE LOADING ISSUE
+//------------------------------------------------------------------
+
+			_background_texture.loadFromFile("assets/pictures/background.jpg");
+			_background_sprite.setTexture(_background_texture);
+
+			_s_buffer_main->loadFromFile("assets/audio/theme.wav");
+			_s_sound_main->setBuffer(*_s_buffer_main);
+			_s_sound_main->setLoop(true);
+
+			_s_buffer_option_hover->loadFromFile("assets/audio/hover.wav");
+			_s_sound_other->setBuffer(*_s_buffer_option_hover);
+
 			_txt_font.loadFromFile("assets/fonts/japanese.ttf");
 
-			_items[_TITLE].r_size = sf::Vector2f(_WIDTH, _HEIGHT*0.2);
-			_items[_TITLE].r_position = sf::Vector2f(_WIDTH*0.3, 0);
+			_items[_TITLE].r_size = sf::Vector2f(_WIDTH*0.8, _HEIGHT*0.2);
+			_items[_TITLE].r_position = sf::Vector2f(_WIDTH*0.2, 0);
 			_items[_TITLE].b_color = sf::Color(200,200,200,0);
 			_items[_TITLE].t_color = sf::Color(255,255,255,150);
 			_items[_TITLE].label = "TICTACTOE";
-			_items[_TITLE].t_size = 100;
 
 			_items[_OFFLINE].r_size = sf::Vector2f(_WIDTH*0.3, _HEIGHT*0.2);
-			_items[_OFFLINE].r_position = sf::Vector2f(_WIDTH*0.1, _HEIGHT*0.3);
+			_items[_OFFLINE].r_position = sf::Vector2f(_WIDTH*0.1, _HEIGHT*0.4);
 			_items[_OFFLINE].label = "OFFLINE";
-			_items[_OFFLINE].t_size = 50;
 
 			_items[_ONLINE].r_size = sf::Vector2f(_WIDTH*0.3, _HEIGHT*0.2);
-			_items[_ONLINE].r_position = sf::Vector2f(_WIDTH*0.8, _HEIGHT*0.3);
+			_items[_ONLINE].r_position = sf::Vector2f(_WIDTH*0.6, _HEIGHT*0.4);
 			_items[_ONLINE].label = "ONLINE";
-			_items[_ONLINE].t_size = 50;
 
 			_items[_EXIT].r_size = sf::Vector2f(_WIDTH*0.1, _HEIGHT*0.1);
-			_items[_EXIT].r_position = sf::Vector2f(_WIDTH*0.9, _HEIGHT*0.7);
+			_items[_EXIT].r_position = sf::Vector2f(_WIDTH*0.8, _HEIGHT*0.8);
 			_items[_EXIT].label = "EXIT";
-			_items[_EXIT].t_size = 50;
 		}
 		void _updateEntities(){
-			if(!(_s_sound->getStatus() == sf::Sound::Playing))
-				_s_sound->play();
-			for (int i=_TITLE; i<=_EXIT;i++){
-
-			}
+			if(!(_s_sound_main->getStatus() == sf::Sound::Playing))
+				_s_sound_main->play();
 		}
 		GameState handleInput(){
 			if(_event->type==sf::Event::MouseButtonPressed){
@@ -75,6 +91,9 @@ class CMenu{
 					_rect_obj.setPosition(_items[i].r_position);
 					_rect_obj.setSize(_items[i].r_size);
 					if(_rect_obj.getGlobalBounds().contains(static_cast<sf::Vector2f>(sf::Mouse::getPosition(*_window)))){
+						_s_buffer_option_hover->loadFromFile("assets/audio/select.wav");
+						_s_sound_other->setBuffer(*_s_buffer_option_hover);
+						_s_sound_other->play();
 						return static_cast<GameState>(i+1);
 					}
 				}
@@ -87,20 +106,26 @@ class CMenu{
 
 			_updateEntities();
 
+//			_window->draw(_background_sprite);
+
 			for(int i=_TITLE; i <= _EXIT; i++){
 				_rect_obj.setSize(_items[i].r_size);
 				_rect_obj.setPosition(_items[i].r_position);
 				_rect_obj.setSize(_items[i].r_size);
 
-				//Change color if rect object contains mouse pointer
+				//Change color if rect object contains mouse pointer and play hover sound
 				if (i != _TITLE){
 					if(_rect_obj.getGlobalBounds().contains(static_cast<sf::Vector2f>(sf::Mouse::getPosition(*_window)))){
+						if(_s_sound_other->getStatus() != sf::Sound::Playing && !_hover_played){
+							_s_sound_other->play();
+							_hover_played = true;
+						}
 						_items[i].b_color = sf::Color(200,200,200,50);
 						_items[i].t_color = sf::Color::Cyan;
-
 					}else{
-						_items[i].b_color = sf::Color(200,200,200,150);
-						_items[i].t_color = sf::Color::Black;
+						_hover_played = false;
+						_items[i].b_color = sf::Color(200,200,200,10);
+						_items[i].t_color = sf::Color(200,0,0);
 					}
 				}
 				_rect_obj.setFillColor(_items[i].b_color);
